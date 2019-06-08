de69acd6beefea98e3c99c37e636e5aa8f871afc
clamyang <ybq2888@163.com>
2019-06-08 20:57:42 +0800

    重新配置

--- comment/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Comment
-
-# Register your models here.
-
-class CommentAdmin(admin.ModelAdmin):
-	list_display = ('id', 'comment_content', 'cteated_time', 'content_type', 'user', 'root', 'parent')  # content_object显示对应的对象
-
-admin.site.register(Comment, CommentAdmin)
