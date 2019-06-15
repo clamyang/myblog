927622da3044bd9175c802a5f6dd34de15f532db
clamyang <ybq2888@163.com>
2019-06-15 21:22:11 +0800

    添加信号通知的url分支

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,17 +1,17 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+	https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+	1. Add an import:  from my_app import views
+	2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+	1. Add an import:  from other_app.views import Home
+	2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+	1. Import the include() function: from django.urls import include, path
+	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
@@ -24,15 +24,16 @@ app_name = 'home_page'
 urlpatterns = [
 	# 首页
 	path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
-    # ckeditor（富文本编辑器）的默认配置信息
-    path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('comment/', include('comment.urls')),  # 评论路由
-    path('likes/', include('likes.urls')),  # 点赞路由
-    path('user/', include('user.urls')),
-    path('notifications/', include('notifications.urls'), name='notifications'),
-    path('my_notifications/', views.my_notifications, name='my_notifications')
+	path('admin/', admin.site.urls),
+	path('blog/', include('blog.urls')),
+	# ckeditor（富文本编辑器）的默认配置信息
+	path('ckeditor/', include('ckeditor_uploader.urls')),
+	path('comment/', include('comment.urls')),  # 评论路由
+	path('likes/', include('likes.urls')),  # 点赞路由
+	path('user/', include('user.urls')),
+	path('notifications/', include('notifications.urls'), name='notifications'),
+	path('my_notifications/', include('my_notifications.urls'), name='my_notifications'),
+	path('search/', views.search, name='search'),
 ]
 
 # 配置MEDIA的存储路径。
